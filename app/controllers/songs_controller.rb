b2cbc2599dd4a59dbe383206eddb2708a9d544bc
Cheng Vue <cvu3@ucsc.edu>
2019-01-09 06:36:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,7 +5,7 @@ class SongsController < ApplicationController
     erb :"songs/index"
   end
 
-  post "/songs/:slug" do
+  get "/songs/:slug" do
     @song = Song.find_by_slug(params[:slug])
     binding.pry
     erb :"songs/show"
