042351d00527860f152f24e0a8f2c1257a3b4599
Cheng Vue <cvu3@ucsc.edu>
2019-01-09 07:15:39 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+artist1 = Artist.new(name: "Mike")
+song1 = Song.new(name: "Hellu")
+genre1 = Genre.new(name: "pop")
+song1.artist = artist1
+song1.genre = genre1
