b8a6f49aaf32d1e7b03759b254c3ff01c07c7191
Cheng Vue <cvu3@ucsc.edu>
2019-01-09 05:40:22 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -10,6 +10,6 @@ class Genre < ActiveRecord::Base
 
   def self.find_by_slug(slug)
     genre_name = slug.split("-").map(&:capitalize).join(' ')
-    genre = Song.find_by(name: genre_name)
+    genre = Genre.find_by(name: genre_name)
   end
 end
