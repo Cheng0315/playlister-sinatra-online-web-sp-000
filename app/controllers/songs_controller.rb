a561e921618829dd176bf19dc0767fd0143e28c1
Cheng Vue <cvu3@ucsc.edu>
2019-01-09 08:42:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,14 +14,14 @@ class SongsController < ApplicationController
     if !!@artist
       @song = Song.new(name: params[:Name])
       @song.artist = @artist
-      @genre = Genre.find_by(name: params[:genres])
+      @genre = Genre.find_by(name: params[:genres][])
       @song.genres << @genre
       @song.save
     else
       @artist = Artist.create(name: params["Artist Name"])
       @song = Song.new(name: params[:Name])
       @song.artist = @artist
-      @genre = Genre.find_by(name: params[:genres])
+      @genre = Genre.find_by(name: params[:genres][])
       @song.genres << @genre
       @song.save
     end
