96c53b9b2b2dcc486225186d6bbd7a21e9b4e5b4
Cheng Vue <cvu3@ucsc.edu>
2019-01-09 07:38:47 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -6,6 +6,7 @@ class GenresController < ApplicationController
 
   get "/genres/:slug" do
     @genre = Genre.find {|genre| genre.slug == params[:slug]}
+    binding.pry
     erb :"genres/show"
   end
 end
