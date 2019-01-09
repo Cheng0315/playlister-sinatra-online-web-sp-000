32482551128573e156c1bf3e37c1467917a051bd
Cheng Vue <cvu3@ucsc.edu>
2019-01-09 05:33:15 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres through: :song_genres
+
+  def slug
+    song_name = self.name
+    song_name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    song_name = slug.split("-").map(&:capitalize).join(' ')
+    song = Song.find_by(name: song_name)
+  end
 end
