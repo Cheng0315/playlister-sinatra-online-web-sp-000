a148e3a31afca6231b4cbcafc050ebaf1705a0d1
Cheng Vue <cvu3@ucsc.edu>
2019-01-09 06:52:46 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,5 +11,6 @@ class Song < ActiveRecord::Base
   def self.find_by_slug(slug)
     song_name = slug.split("-").map(&:capitalize).join(' ')
     song = self.find_by(name: song_name)
+    song
   end
 end
